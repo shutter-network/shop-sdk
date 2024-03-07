8fcbb7a428996296d75b8a0b054f59daf27465e9
Konrad Feldmeier <konrad@brainbot.com>
2024-03-07 14:25:44 +0100

    Fix rlp encoding error for value=0
    Prepare 0.1.3 release

--- lib.esm/index.d.ts
+++ lib.esm/index.d.ts
@@ -1,4 +1,4 @@
-export { ShutterProvider } from './provider-shutter.js';
-export { ethers, Wallet, BrowserProvider } from 'ethers';
-export { init, decrypt } from '@shutter-network/shutter-crypto';
+export { ShutterProvider } from './provider-shutter.js'
+export { ethers, Wallet, BrowserProvider } from 'ethers'
+export { init, decrypt } from '@shutter-network/shutter-crypto'
 //# sourceMappingURL=index.d.ts.map
--- lib.esm/signer-shutter.d.ts
+++ lib.esm/signer-shutter.d.ts
@@ -1,21 +1,28 @@
-import { JsonRpcSigner, TransactionRequest, BigNumberish } from 'ethers';
-import { ShutterProvider } from './provider-shutter';
-import { currentEnv, EnvSpecificArg } from './types';
+import { JsonRpcSigner, TransactionRequest, BigNumberish } from 'ethers'
+import { ShutterProvider } from './provider-shutter'
+import { currentEnv, EnvSpecificArg } from './types'
 export declare class SignerShutter extends JsonRpcSigner {
-    wasmUrl: EnvSpecificArg<typeof currentEnv>;
-    keyperSetManagerAddress: string;
-    inboxAddress: string;
-    keyBroadcastAddress: string;
-    constructor(provider: ShutterProvider, address: string);
-    isShutterPaused(): Promise<boolean>;
-    getCurrentEonKey(): Promise<string>;
-    getEonKeyForBlock(block: number): Promise<string>;
-    getEonForBlock(block: number): Promise<number>;
-    getEonKey(eon: number): Promise<string>;
-    hexKeyToArray(hexvalue: string): Uint8Array;
-    decodeExecutionReceipt(receipt: string): any;
-    encryptOriginalTx(_tx: TransactionRequest, inclusionBlock: number): Promise<[Uint8Array, BigNumberish]>;
-    _sendTransactionTrace(tx: TransactionRequest, inclusionWindow?: number, blockProvider?: any): Promise<any>;
-    sendTransaction(tx: TransactionRequest): Promise<any>;
+  wasmUrl: EnvSpecificArg<typeof currentEnv>
+  keyperSetManagerAddress: string
+  inboxAddress: string
+  keyBroadcastAddress: string
+  constructor(provider: ShutterProvider, address: string)
+  isShutterPaused(): Promise<boolean>
+  getCurrentEonKey(): Promise<string>
+  getEonKeyForBlock(block: number): Promise<string>
+  getEonForBlock(block: number): Promise<number>
+  getEonKey(eon: number): Promise<string>
+  hexKeyToArray(hexvalue: string): Uint8Array
+  decodeExecutionReceipt(receipt: string): any
+  encryptOriginalTx(
+    _tx: TransactionRequest,
+    inclusionBlock: number,
+  ): Promise<[Uint8Array, BigNumberish]>
+  _sendTransactionTrace(
+    tx: TransactionRequest,
+    inclusionWindow?: number,
+    blockProvider?: any,
+  ): Promise<any>
+  sendTransaction(tx: TransactionRequest): Promise<any>
 }
 //# sourceMappingURL=signer-shutter.d.ts.map
